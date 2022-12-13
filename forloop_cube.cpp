0fb3852a0abe100d52c358768899023b4bff4efd
Abhishek <120470695+upadhayaabhi@users.noreply.github.com>
2022-12-13 18:15:09 +0530

    Add files via upload

---
+++ forloop_cube.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int i,n,a;
+	printf("Enter the number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+//		a=i*i*i;
+	printf("cube of %d=%d\n",i,i*i*i);
+	}
+//		printf("\n");
+}
